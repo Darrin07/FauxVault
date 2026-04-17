a7f892d8925446b3a5ea1be5a6d2706717251878
Felicia Barrett <barretfe@engr.orst.edu>
2026-04-17 20:51:39 +0100

    General implementation plan for main, index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDom.createRoot(document.getElementById('root')).render{
+  <React.StrictMode>
+    /**Browser Routers*/
+    /**Vulnerability  */
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+}
