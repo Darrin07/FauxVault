2f17aef72f86c30404e313e0e4d8b2eaedcb37a8
Darrin07 <102775335+Darrin07@users.noreply.github.com>
2026-04-21 14:43:54 -0400

    Update server/src/routes/settings.js
    correction: updated name form sController to settingsController for readability.
    
    Co-authored-by: Nicholas Noochla-or <nnlaor@gmail.com>

--- server/src/routes/settings.js
+++ server/src/routes/settings.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const sController = require('../controllers/settingsController');
+const settingsController = require('../controllers/settingsController');
 
-
-router.get('/', sController.getSettings);
-router.post('/', sController.updateSettingHandler);
-
+router.get('/', settingsController.getSettings);
+router.post('/', settingsController.updateSettingHandler);
 module.exports = router;
