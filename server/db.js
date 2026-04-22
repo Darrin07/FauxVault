34dac7112b367218a147077f54c79133567f3085
ethandavidlee <ethandavid.edl@gmail.com>
2026-04-22 17:43:06 +0100

    Fixed db.js (missing comma)

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Manage connections
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST || 'localhost' // fallback to localhost
+  host: process.env.DB_HOST || 'localhost', // fallback to localhost
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
