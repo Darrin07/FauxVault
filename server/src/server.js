cf287cc61bf458f8f5ddf1c45ebca0c965318941
ethandavidlee <ethandavid.edl@gmail.com>
2026-04-22 17:42:16 +0100

    Connected express server to docker postgres, cleaned db sql files, removed redundance package.json

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const config = require('./config');
+const pool = require('../db');
 
 app.listen(config.port, () => {
   console.log(`FauxVault API running on port ${config.port}`);
